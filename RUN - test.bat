7dd2e83be34f29f9f3500b0abb6719e71bcfbb59
Watanuki-Kimihiro <47470844+Watanuki-Kimihiro@users.noreply.github.com>
2019-08-28 22:09:30 +0800

    Add files via upload

---
+++ RUN - test.bat
@@ -0,0 +1,3 @@
+CALL D:\ProgramData\Anaconda3\Scripts\activate.bat D:\ProgramData\Anaconda3
+python test.py
+pause
---
+++ 打包.bat
@@ -0,0 +1,3 @@
+CALL D:\ProgramData\Anaconda3\Scripts\activate.bat D:\ProgramData\Anaconda3
+pyinstaller -F -w -i favicon_128x128.ico test.py
+pause
